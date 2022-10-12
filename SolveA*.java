b28ca8732fd0b4a8d642de6a67ce4fda197365af
AmberDassen <115467169+AmberDassen@users.noreply.github.com>
2022-10-12 22:27:25 +0200

    Update SolveA*.java

--- SolveA*.java
+++ SolveA*.java
@@ -1,16 +1,7 @@
 public class AStar {
   
   
-  //moves the tile and checks is the move is valid
-	private move(Tile T){
-		
-		int dx = Math.abs(blank.xCoord - T.xCoord);
-		int dy = Math.abs(blank.yCoord - T.yCoord);
-		
-		if(dx + dy = 1 && T.xCoord >= 0 && T.xCoord < boardSize && T.yCoord >= 0 && T.yCoord < boardSize){
-			
-		}
-	}
+
   
 }
 
